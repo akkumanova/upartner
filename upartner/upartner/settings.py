e3793de3b80dafd20f5e1279566cf1850771c4b7
akkumanova <akkumanova@gmail.com>
2015-12-14 22:25:00 +0200

    Increase auto generated partner passwords length to 10 chars

--- upartner/upartner/settings.py
+++ upartner/upartner/settings.py
@@ -9,7 +9,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-PASSWORD_LENGTH = 6
+PASSWORD_LENGTH = 10
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
